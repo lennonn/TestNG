10b6efe4d62b1e23f7ab03fec22a3142d694a729
zlennon <zl1172_2011@163.com>
2018-01-16 10:09:26 +0800

    first commit

---
+++ src/test/java/com/zl/lennon/AppTest.java
@@ -0,0 +1,11 @@
+package com.zl.lennon;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void main() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
---
+++ src/test/java/com/zl/lennon/regex/RegextTest.java
@@ -0,0 +1,22 @@
+package com.zl.lennon.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class RegextTest {
+
+	@Test
+	public void test() {
+		String regex ="('|')(.*?)(\1)";
+		Pattern p=Pattern.compile(regex); 
+		String str ="'Hello', 'World'";
+		Matcher re=p.matcher(str);
+		 while(re.find()){
+			 System.out.println(re.group());
+		 }
+
+	}
+
+}
